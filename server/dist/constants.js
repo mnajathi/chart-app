fb35b9bb0177216928595a75ac271c10015a6221
mohamed.najathi@prodoscore.com <najathi777@gmail.com>
2023-08-14 13:33:17 +0530

    update gitignore files

--- server/dist/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.__prod__ = exports.__port__ = void 0;
-exports.__port__ = process.env.PORT;
-exports.__prod__ = process.env.NODE_ENV === 'production';
-//# sourceMappingURL=constants.js.map
