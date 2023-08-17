7ecd18c41d275e96a0763a67c7ec9a49b09987ac
mohamed.najathi@prodoscore.com <najathi777@gmail.com>
2023-08-17 14:08:15 +0530

    tweak

---
+++ web/__mocks__/fileMock.ts
@@ -0,0 +1,7 @@
+module.exports = {
+	src: '/img.jpg',
+	height: 24,
+	width: 24,
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	blurDataURL: 'data:image/png;base64,imagedata',
+};
